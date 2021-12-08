f59b04914e680dd4d409a53d7b4d21b5d2c42307
Andrew Wilkinson <andywilkinson1993@gmail.com>
2021-12-08 22:49:42 +0000

    contracts

--- contracts/send_f_faucet.sol
+++ contracts/send_f_faucet.sol
@@ -1,46 +1,38 @@
-pragma solidity 0.4.24;
+// SPDX-License-Identifier: GPL-3.0
 
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
-import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+pragma solidity >=0.7.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract Faucet {
     using SafeMath for uint256;
 
     ERC20 public token;
-    address public wallet;
-    uint256 public rate;
-    uint256 public weiRaised;
+    uint256 public amount;
     mapping(address => uint256) public startTime;
 
     constructor(
         uint256 _amount,
-        address _wallet,
         ERC20 _token
-    ) public {
+    ) {
         require(_amount > 0);
-        require(_wallet != address(0));
-        require(_token != address(0));
-
         amount = _amount;
-        wallet = _wallet;
         token = _token;
     }
 
-    function() external payable {
-        buyTokens(msg.sender);
-    }
-
-    function hasElapsed() public returns (bool hasElapsed) {
-        if (now >= startTime[msg.sender] + 10 minutes) {
+    function hasElapsed() private returns (bool) {
+        if (block.timestamp >= startTime[msg.sender] + 10 minutes) {
             // 10 minutes has elapsed from startTime[msg.sender]
             return true;
         }
         return false;
     }
 
-    function claim(address _beneficiary) public {
-        require(_beneficiary != address(0));
-        token.transfer(_beneficiary, amount);
-        startTime[msg.sender] = now;
+    function claim() public {
+        require(msg.sender != address(0));
+        require(hasElapsed());
+        token.transfer(msg.sender, amount);
+        startTime[msg.sender] = block.timestamp;
     }
 }
--- contracts/send_f_token.sol
+++ contracts/send_f_token.sol
@@ -1,11 +1,17 @@
-// SDPX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: GPL-3.0
 
-import "OpenZeppelin/openzeppelin-contracts@4.0.0/contracts/token/ERC20/ERC20.sol";
+pragma solidity >=0.7.0 <0.9.0;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract SendFToken is ERC20 {
+contract SendFToken is ERC20, Ownable {
     constructor(uint256 initialSupply) ERC20("SendFToken", "F") {
         _mint(msg.sender, initialSupply);
+    }
+
+    function reloadFaucet(address faucet) public onlyOwner  {
+        require(this.balanceOf(address(this)) < 1000000);
+        _mint(faucet, 10000000);
     }
 }
