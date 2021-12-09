6e9127a4f5d29582a7b250d9ffe945c0b3a55c4e
Andy <andrew.wilkinson@r3.com>
2021-12-09 00:46:34 +0000

    contracts

--- contracts/send_f_token.sol
+++ contracts/send_f_token.sol
@@ -6,12 +6,20 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract SendFToken is ERC20, Ownable {
-    constructor(uint256 initialSupply) ERC20("SendFToken", "F") {
-        _mint(msg.sender, initialSupply);
+    mapping(address => uint256) public startTime;
+    constructor() ERC20("SendFToken", "F") {
+     
     }
-
-    function reloadFaucet(address faucet) public onlyOwner  {
-        require(this.balanceOf(address(this)) < 1000000);
-        _mint(faucet, 10000000);
+    function hasElapsed() private returns (bool) {
+        if (block.timestamp >= startTime[msg.sender] + 60 minutes) {
+            return true;
+        }
+        return false;
+    }
+    function claim() public {
+        require(msg.sender != address(0));
+        require(hasElapsed());
+        _mint(msg.sender, 100000000000000000000);
+        startTime[msg.sender] = block.timestamp;
     }
 }
