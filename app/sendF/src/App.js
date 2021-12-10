c9c6173571673106b1ffca4ec7af3b7df8bf5c75
Andy <andrew.wilkinson@r3.com>
2021-12-10 11:22:57 +0000

    styles

--- app/sendF/src/App.js
+++ app/sendF/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
-import Minter from './Minter'
+import Minter from './Minter';
 
 function App() {
   return (
-    <div className="App" style={{ backgroundColor: '#344771f7', height: '100vh' }}>
-      <div style={{ paddingTop: '15vh' }}></div>
+    <div
+      className="App"
+      style={{
+        backgroundColor: '#2d3445f7',
+        height: '100vh',
+        backgroundImage:
+          'url("https://gumlet.assettype.com/afkgaming%2Fimport%2Fmedia%2Fimages%2F92881-3fb2db6cccf4a23383383394b28b2b31.jpeg?auto=format%2Ccompress&dpr=1.0&w=1200")',
+        backgroundRepeat: 'no-repeat',
+        backgroundAttachment: 'fixed',
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+      }}
+    >
+      <div style={{ paddingTop: '2em' }}></div>
       <Minter></Minter>
     </div>
   );
--- app/sendF/src/Minter.js
+++ app/sendF/src/Minter.js
@@ -42,17 +42,20 @@ const Minter = (props) => {
     }
   }
 
-  const connectWalletPressed = async () => {
+  const connectWalletPressed = async (e) => {
+    e.preventDefault();
     const walletResponse = await connectWallet();
     setStatus(walletResponse.status);
     setWallet(walletResponse.address);
   };
 
-  const addTokenPressed = async () => {
+  const addTokenPressed = async (e) => {
+    e.preventDefault();
     await addToken();
   };
 
-  const onMintPressed = async () => {
+  const onMintPressed = async (e) => {
+    e.preventDefault();
     const { success, status } = await mintNFT(url, name, description);
     setStatus(status);
     if (success) {
@@ -63,46 +66,50 @@ const Minter = (props) => {
   };
 
   return (
-    <div style={{ backgroundColor: '#f0f0f0', borderRadius: '15px' }} className="Minter">
-      <button style={{ color: 'purple', borderColor: 'purple' }} id="walletButton" onClick={connectWalletPressed}>
+    <div style={{ backgroundColor: '#fff', borderRadius: '15px' }} className="Minter">
+      <button style={{ color: '#8247e5', borderColor: '#8247e5' }} id="walletButton" onClick={connectWalletPressed}>
         {walletAddress.length > 0 ? (
           'Connected: ' + String(walletAddress).substring(0, 6) + '...' + String(walletAddress).substring(38)
         ) : (
           <span>Connect Wallet</span>
         )}
       </button>
-
       <br></br>
-      <h1 id="title" style={{ color: '#344771f7' }}>
-        Send F Token <span style={{ color: 'purple'}}>(Polygon)</span>
+      <h1 id="title" style={{ color: '#2d3445f7' }}>
+        Send F Token <span style={{ color: '#8247e5' }}>(Polygon)</span>
       </h1>
-      <p style={{ color: '#344771f7' }}>
-        <strong>Claim 100 tokens every hour. Send F to pay respects to those in need.</strong>
-      </p>
+      <p style={{ color: '#2d3445f7' }}>Claim 100 tokens every hour. Send F tokens to pay respects.</p>
       <form>
-        <h2 style={{ color: '344771f7' }}>Contract </h2>
+        <h3 style={{ color: '#2d3445f7' }}>Contract </h3>
+
         <a
           href="https://polygonscan.com/address/0x065902d124b823BC237890be37832d1790DFfc32"
           target="_blank"
           rel="noreferrer"
-          style={{ color: 'purple' }}
+          style={{ color: '#8247e5', overflowWrap: 'break-word' }}
         >
-          polygonscan.com/address/0x065902d124b823BC237890be37832d1790DFfc32
+          0x065902d124b823BC237890be37832d1790DFfc32
         </a>
-        <h2 style={{ color: '344771f7' }}>Add F Token to MetaMask</h2>
-        <button id="addButton" style={{ color: 'red', borderColor: 'red', width: '100px' }} onClick={addTokenPressed}>
-          🦊
+
+        <h3 style={{ color: '#2d3445f7' }}>Add F Token to MetaMask</h3>
+        <button
+          id="addButton"
+          style={{ color: 'red', border: 'none', width: '70px', padding: '0px' }}
+          onClick={addTokenPressed}
+        >
+          <img src="https://static.coingecko.com/s/metamask_fox-11b1aab7f9a07cbe8903d8d6eb1e6d42be66d1bdd838c10786c1c49a2efb36f0.svg"></img>
         </button>
-        <h2 style={{ color: '#344771f7' }}>Mint 100 F Tokens</h2>
+        <h3 style={{ color: '#2d3445f7' }}>Mint 100 F Tokens</h3>
       </form>
-      <button
-        id="mintButton"
-        style={{ backgroundColor: '#344771f7', borderColor: '#344771f7', width: '100px' }}
-        onClick={onMintPressed}
-      >
-        F
+      <button id="mintButton" style={{ border: 'none', width: '70px', padding: '0px' }} onClick={onMintPressed}>
+        <img
+          style={{ maxWidth: '70px' }}
+          src="https://emoji.discord.st/emojis/10298264-ad02-44d6-8791-42b8aeca875c.png"
+        ></img>
       </button>
-      <p id="status">{status}</p>
+      <p id="status" style={{ color: '#2d3445f7', overflowWrap: 'break-word' }}>
+        {status}
+      </p>
     </div>
   );
 };
